08624e822d16542b4536f84efe749940546bc76e
Sun PeiFeng <g7733pf@gmail.com>
2013-12-13 17:06:46 +0800

    Initial Commit

---
+++ stackAccordingToTextBook/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  stackAccordingToTextBook
+//
+//  Created by 孙培峰 on 1312/13/.
+//  Copyright (c) 2013 孙培峰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
